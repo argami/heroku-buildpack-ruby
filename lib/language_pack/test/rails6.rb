33a571dba3e70ceee058b44bb3f343b223a0d080
Gamaliel Toro <argami@gmail.com>
2021-02-14 21:47:25 +0100

    using a custom db rake to prepare db on tests

--- lib/language_pack/test/rails6.rb
+++ lib/language_pack/test/rails6.rb
@@ -3,7 +3,7 @@ class LanguagePack::Rails6
   private 
 
   def db_prepare_test_rake_tasks
-    ["db:schema:load_if_ruby", "db:migrate"].map {|name| rake.task(name) }
+    ["db:custom_prepare"].map {|name| rake.task(name) }
   end
 
 end
